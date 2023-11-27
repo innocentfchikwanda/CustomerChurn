65df82eb3079e250df5dab03aed55cca9e6810f7
innocentfchikwanda <115183478+innocentfchikwanda@users.noreply.github.com>
2023-11-27 09:05:27 +0000

    Update churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -1,118 +1,76 @@
-import subprocess
+import pickle
 import pandas as pd
 import numpy as np
 import streamlit as st
-import sklearn
-import joblib
 
-# Update pip
-subprocess.run(['pip', 'install', '--upgrade', 'pip'])
+# Load the pickled model
+model = pickle.load(open('churn_model.pkl', 'rb'))
 
-# Install required packages
-subprocess.run(['pip', 'install', 'keras==2.15.0', 'scikit-learn==1.3.2', 'tensorflow==2.14.0'])
+# Define the function to make predictions
+def predict_churn(data):
+  # Convert data to a DataFrame
+  data = pd.DataFrame(data)
 
-# Specify the path to your .joblib file
-model_filename = 'churn_model.joblib'
+  # Standardize the data
+  scaler = StandardScaler()
+  scaled_data = scaler.fit_transform(data)
 
-# Load the model from the specified file
-model_y = joblib.load(model_filename)
+  # Make predictions
+  predictions = model.predict(scaled_data)
 
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-label_encoder = LabelEncoder()
+  # Convert predictions to probabilities
+  probabilities = model.predict_proba(scaled_data)
 
-# Streamlit app
-def main():
-    st.title("Churn Prediction App")
-    st.write("Enter customer details to predict churn.")
+  return predictions, probabilities
 
-    # User input
-    gender_options = ['Male', 'Female']
-    partner_options = ['No', 'Yes']
-    dependents_options = ['No', 'Yes']
-    phone_service_options = ['Yes', 'No']
-    multiple_lines_options = ['No', 'No phone service', 'Yes']
-    internet_service_options = ['DSL', 'Fiber optic', 'No']
-    online_security_options = ['No', 'Yes', 'No internet service']
-    online_backup_options = ['No', 'Yes', 'No internet service']
-    device_protection_options = ['No', 'Yes', 'No internet service']
-    tech_support_options = ['No', 'Yes', 'No internet service']
-    senior_citizen_options = ["Yes", "No"]
-    streaming_tv_options = ['No', 'No internet service', 'Yes']
-    streaming_movies_options = ['No', 'Yes', 'No internet service']
-    contract_options = ['Month-to-month', 'One year', 'Two year']
-    paperless_billing_options = ['No', 'Yes']
-    payment_method_options = ['Electronic check', 'Mailed check','Bank transfer (automatic)','Credit card (automatic)']
+# Create the Streamlit app
+st.title('Churn Prediction App')
 
-    gender = st.selectbox('Gender', gender_options)
-    partner = st.selectbox('Partner', partner_options)
-    dependents = st.selectbox('Dependents', dependents_options)
-    phone_service = st.selectbox('Phone Service', phone_service_options)
-    multiple_lines = st.selectbox('Multiple Lines', multiple_lines_options)
-    internet_service = st.selectbox('Internet Service', internet_service_options)
-    online_security = st.selectbox('Online Security', online_security_options)
-    online_backup = st.selectbox('Online Backup', online_backup_options)
-    device_protection = st.selectbox('Device Protection', device_protection_options)
-    tech_support = st.selectbox('Tech Support', tech_support_options)
-    senior_citizen = st.selectbox('SeniorCitizen', senior_citizen_options)
-    streaming_tv = st.selectbox('Streaming TV', streaming_tv_options)
-    streaming_movies = st.selectbox('Streaming Movies', streaming_movies_options)
-    contract = st.selectbox('Contract', contract_options)
-    paperless_billing = st.selectbox('Paperless Billing', paperless_billing_options)
-    payment_method = st.selectbox('Payment Method', payment_method_options)
+# Collect user input
+st.header('Customer Information')
+gender = st.selectbox('Gender', ['Male', 'Female'])
+senior_citizen = st.selectbox('Senior Citizen', ['No', 'Yes'])
+partner = st.selectbox('Partner', ['Yes', 'No'])
+dependents = st.selectbox('Dependents', ['Yes', 'No'])
+tenure = st.number_input('Tenure')
+internet_service = st.selectbox('Internet Service', ['No', 'DSL', 'Fiber Optic'])
+online_security = st.selectbox('Online Security', ['No', 'Yes', 'No internet service'])
+online_backup = st.selectbox('Online Backup', ['No', 'Yes', 'No internet service'])
+device_protection = st.selectbox('Device Protection', ['No', 'Yes', 'No internet service'])
+tech_support = st.selectbox('Tech Support', ['No', 'Yes', 'No internet service'])
+streaming_movies = st.selectbox('Streaming Movies', ['No', 'Yes', 'No internet service'])
+streaming_tv = st.selectbox('Streaming TV', ['No', 'Yes', 'No internet service'])
+monthly_charges = st.number_input('Monthly Charges')
+total_charges = st.number_input('Total Charges')
 
-    # Numeric inputs
-    monthly_charges = st.number_input('Monthly Charges', min_value=0)
-    total_charges = st.number_input('Total Charges', min_value=0)
-    tenure = st.number_input('Tenure (months)', min_value=0)
+# Prepare the data for prediction
+data = {
+  'gender': gender,
+  'senior_citizen': senior_citizen,
+  'partner': partner,
+  'dependents': dependents,
+  'tenure': tenure,
+  'internet_service': internet_service,
+  'online_security': online_security,
+  'online_backup': online_backup,
+  'device_protection': device_protection,
+  'tech_support': tech_support,
+  'streaming_movies': streaming_movies,
+  'streaming_tv': streaming_tv,
+  'monthly_charges': monthly_charges,
+  'total_charges': total_charges
+}
 
-    # Convert user input to a DataFrame
-    user_input = {
-        'gender': gender,
-        'Partner': partner,
-        'Dependents': dependents,
-        'PhoneService': phone_service,
-        'MultipleLines': multiple_lines,
-        'InternetService': internet_service,
-        'OnlineSecurity': online_security,
-        'OnlineBackup': online_backup,
-        'DeviceProtection': device_protection,
-        'TechSupport': tech_support,
-        'SeniorCitizen': senior_citizen,
-        'StreamingTV': streaming_tv,
-        'StreamingMovies': streaming_movies,
-        'Contract': contract,
-        'PaperlessBilling': paperless_billing,
-        'PaymentMethod': payment_method,
-        'MonthlyCharges': monthly_charges,
-        'TotalCharges': total_charges,
-        'tenure': tenure,
-    }
+# Make predictions
+predictions, probabilities = predict_churn(data)
 
-    input_df = pd.DataFrame([user_input])
-    
-    numeric = input_df.iloc[:,17:19]
-    numeric = pd.DataFrame(numeric)
-    numeric["tenure"] = input_df.iloc[:,18:]
-    non_numeric = customers.drop(["tenure", "MonthlyCharges", "TotalCharges"], axis = 1)
-    
-    for column in non_numeric.columns:
-        if non_numeric[column].dtype == 'object':  # Check if the column is of object type (categorical)
-            non_numeric[column] = label_encoder.fit_transform(non_numeric[column])
-        
-    non_numeric = non_numeric.drop(["StreamingTV", "StreamingMovies", "PhoneService"], axis=1)
-    
-    x = pd.concat([non_numeric, numeric], axis=1)
-    print(x.shape)
+# Display the predictions
+if predictions == 0:
+  st.success('Customer is not likely to churn')
+else:
+  st.error('Customer is likely to churn')
 
-    x.head(2)
-    # Make predictions
-    prediction = model_y.predict(x)
-
-
-    # Display the prediction
-    st.write("Churn Prediction:")
-    st.write(prediction)
-
-
-if __name__ == '__main__':
-    main()
+# Display the probabilities
+st.subheader('Prediction Probabilities')
+st.write('Churn: {:.2f}'.format(probabilities[0][1]))
+st.write('Not Churn: {:.2f}'.format(probabilities[0][0]))
